0aa724dfee3cfdaba7fb6cffbd768181b2a07111
Old Fox <old.fox@mail.ru>
2022-04-04 01:03:46 +0500

    промежуточный коммит

---
+++ Topics/Factory method/Burger store/src/Main.java
@@ -0,0 +1,86 @@
+class TestDrive {
+    public static void main(String[] args) throws InterruptedException {
+        BurgerStore burgerStore = new BurgerStore();
+        burgerStore.orderBurger("chinese");
+        burgerStore.orderBurger("american");
+        burgerStore.orderBurger("russian");
+    }
+}
+
+abstract class BurgerFactory {
+
+    private final long timeSleep = 1500L;
+
+    abstract Burger createBurger(String type);
+
+    Burger orderBurger(String type) throws InterruptedException {
+        Burger burger = createBurger(type);
+        if (burger == null) {
+            System.out.println("Sorry, we are not able to create this kind of burger\n");
+            return null;
+        }
+        System.out.println("Making a " + burger.getName());
+        burger.putBun();
+        burger.putCutlet();
+        burger.putSauce();
+        Thread.sleep(timeSleep);
+        System.out.println("Done a " + burger.getName() + "\n");
+        return burger;
+    }
+}
+
+class BurgerStore extends BurgerFactory {
+    @Override
+    Burger createBurger(String type) {
+        switch (type) {
+            case "chinese": return new ChineseBurger("Chinese Burger");
+            case "american": return new AmericanBurger("American Burger");
+            case "russian": return new RussianBurger("Russian Burger");
+            default:
+                return null;
+        }
+    }
+}
+
+class Burger {
+    private String name;
+
+    Burger(String name) {
+        this.name = name;
+    }
+
+    String getName() {
+        return name;
+    }
+
+    void putBun() {
+        System.out.println("Putting bun");
+    }
+
+    void putCutlet() {
+        System.out.println("Putting cutlet");
+    }
+
+    void putSauce() {
+        System.out.println("Putting sauce");
+    }
+
+}
+
+class ChineseBurger extends Burger {
+    ChineseBurger(String name) {
+        super(name);
+    }
+}
+
+class AmericanBurger extends Burger {
+    AmericanBurger(String name) {
+        super(name);
+    }
+}
+
+class RussianBurger extends Burger {
+    RussianBurger(String name) {
+        super(name);
+    }
+}
