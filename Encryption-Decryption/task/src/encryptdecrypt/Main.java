5f9a4a9f3000f244a4dec21a98dfe3878ef4f3ac
Old Fox <old.fox@mail.ru>
2022-04-03 23:12:31 +0500

    4/6 решено

--- Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -1,20 +1,34 @@
 package encryptdecrypt;
 
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        var direction = scanner.nextLine();
-        var input_message = scanner.nextLine();
-        var inp_key = scanner.nextInt();
+        final String ENC = "enc";
+        final String DEC = "dec";
 
+        final String MODE = "-mode";
+        final String KEY = "-key";
+        final String DATA = "-data";
+
+        var direction = ENC;
+        var message = "";
+        var key = 0;
+
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].contains("-")) {
+                if (MODE.equals(args[i]))
+                    direction = args[++i];
+                else if (KEY.equals(args[i]))
+                    key = Integer.parseInt(args[++i]);
+                else if (DATA.equals(args[i]))
+                    message = args[++i];
+            }
+        }
         Crypto crypto;
-        if ("enc".equals(direction))
-            crypto = new Encrypt(input_message, inp_key);
-        else if ("dec".equals(direction))
-            crypto = new Decrypt(input_message, inp_key);
+        if (ENC.equals(direction))
+            crypto = new Encrypt(message, key);
+        else if (DEC.equals(direction))
+            crypto = new Decrypt(message, key);
         else {
             System.out.println("error!");
             return;
